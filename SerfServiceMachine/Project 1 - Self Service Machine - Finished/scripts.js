1f861536e4ac577ef57db84b5661c127687f2baa
Jose Vilches <josevilches@MacBook-Air-de-Jose.local>
2025-02-08 23:54:15 -0300

    vue upgraded

--- SerfServiceMachine/Project 1 - Self Service Machine - Finished/scripts.js
+++ SerfServiceMachine/Project 1 - Self Service Machine - Finished/scripts.js
@@ -1,63 +1,63 @@
 var products = [
         {
             "photo": "img/big-mac.png",
-            "name": "Big Mac",
+            "name": "Menú 1",
             "price": 5.99,
             "active": false,
             "quantity": 1
         },
         {
             "photo": "img/mc-chicken.png",
-            "name": "Mc Chicken",
+            "name": "Menú 2",
             "price": 4.99,
             "active": false,
             "quantity": 1
         },
         {
             "photo": "img/double-cb.png",
-            "name": "Double Cheese Burger",
+            "name": "Menú 3",
             "price": 2.99,
             "active": false,
             "quantity": 1
         },
         {
             "photo": "img/fries.png",
-            "name": "Fries",
+            "name": "Menú 4",
             "price": 2.99,
             "active": false,
             "quantity": 1
         },
         {
             "photo": "img/nuggets.png",
-            "name": "Mc Nuggets",
+            "name": "Menú 5",
             "price": 3.49,
             "active": false,
             "quantity": 1
         },
         {
             "photo": "img/salad.png",
-            "name": "Salad",
+            "name": "Menú x",
             "price": 2.79,
             "active": false,
             "quantity": 1
         },
         {
             "photo": "img/cola.png",
-            "name": "Coke",
+            "name": "Menú x",
             "price": 1.99,
             "active": false,
             "quantity": 1
         },
         {
             "photo": "img/lipton.png",
-            "name": "Ice Tea",
+            "name": "Menú x",
             "price": 1.99,
             "active": false,
             "quantity": 1
         },
         {
             "photo": "img/water.png",
-            "name": "Water",
+            "name": "Menú x",
             "price": 1.49,
             "active": false,
             "quantity": 1
